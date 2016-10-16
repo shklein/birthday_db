35aca03e11e4bd32f8755652122ff43b89d8bdde
Samantha Klein <saradurga@gmail.com>
2016-10-16 08:27:37 -0500

    placeholder submit page

---
+++ submit.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+  <html>
+    <head>
+      <title>Family Birthdays</title>
+      <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+      <h1>Add a Birthday</h1>
+    </body>
+  </html>
